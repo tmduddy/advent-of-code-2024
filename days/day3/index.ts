32a9af4427ab1030e2b6ff66f2b77a82d3978f9e
tmduddy <login@tylerduddy.com>
2024-12-03 16:14:24 -0500

    day3 part1

--- days/day3/index.ts
+++ days/day3/index.ts
@@ -7,11 +7,25 @@ const input = readFileSync(path.resolve(__dirname, inputFileName), {
   encoding: 'utf8',
   flag: 'r',
 }).split('\n');
-console.log(input);
+// console.log(input);
 
 const part1 = () => {
-  const solution = 0;
+  const mulMatch = /mul\((\d+),(\d+)\)/g;
+  const codeInput = input.join();
+  const matches = [...codeInput.matchAll(mulMatch)];
+  let sum = 0;
+  matches.forEach(match => {
+    const [_, left, right] = match;
+    const leftInt = parseInt(left, 10);
+    const rightInt = parseInt(right, 10);
+    // console.log(_, left, right);
+    const product = leftInt * rightInt;
+    sum += product;
+  });
+
+  const solution = sum;
   console.log(`\nPart 1: ${solution}`);
+  // 28546082 was too low!
 };
 
 const part2 = () => {
