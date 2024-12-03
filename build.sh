4cef75c45c6cc7371aaee26bdff667cb73ac5d5f
tmduddy <login@tylerduddy.com>
2024-12-03 15:55:45 -0500

    update builds ahead of day3

--- build.sh
+++
@@ -1,7 +0,0 @@
-echo "compiling typescript..."
-tsc
-
-echo "copying input data..."
-tar cf - days/**/*.txt | (cd ./dist; tar xf -)
-
-echo "build complete!"
--- runDay.sh
+++
@@ -1,3 +0,0 @@
-DAY_FILE="dist/days/day$1/index.js"
-
-node $DAY_FILE
