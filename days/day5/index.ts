f461c9819a25348158a17e93a4a93628736ec685
tmduddy <login@tylerduddy.com>
2024-12-05 17:50:46 -0500

    day5 part1

--- days/day5/index.ts
+++ days/day5/index.ts
@@ -57,6 +57,53 @@ const part1 = () => {
 };
 
 const part2 = () => {
+  const rules: string[] = [];
+  const updates: string[] = [];
+
+  let isRule = true;
+  input.forEach(inp => {
+    if (inp === '') {
+      isRule = false;
+    } else if (isRule) {
+      rules.push(inp);
+    } else {
+      updates.push(inp);
+    }
+  });
+
+  const rulesObj: Record<number, Set<number>> = {};
+  rules.forEach(rule => {
+    const [left, right] = rule.split('|').map(i => parseInt(i, 10));
+    if (!rulesObj[left]) {
+      rulesObj[left] = new Set([right]);
+    } else {
+      rulesObj[left].add(right);
+    }
+  });
+
+  const badUpdates: number[][] = [];
+  updates.forEach(updateRaw => {
+    const update = updateRaw.split(',').map(p => parseInt(p, 10));
+    let isValid = true;
+    update.forEach((page, idx) => {
+      if (
+        idx > 0 &&
+        isValid &&
+        Object.keys(rulesObj).includes(page.toString())
+      ) {
+        const ruleSet = rulesObj[page];
+        const priorNumbers = [...update].splice(0, idx);
+        isValid = priorNumbers.filter(num => ruleSet.has(num)).length === 0;
+      }
+    });
+    if (!isValid) {
+      badUpdates.push(update);
+    }
+  });
+
+  badUpdates.forEach(update => {
+    console.log(update)
+  })
   const solution = 0;
   console.log(`\nPart 2: ${solution}`);
 };
