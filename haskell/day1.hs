50dcf4afdb139610777fcb62d0476b955ad6ee2d
tmduddy <login@tylerduddy.com>
2024-12-16 09:34:04 -0500

    haskell day1 improved

--- haskell/day1.hs
+++ haskell/day1.hs
@@ -2,17 +2,13 @@ import Data.List (sort)
 
 main = do
   contents <- readFile "../days/day1/input.txt"
-  let strList = getStrList contents
-  let part1 = sum [abs (read x - read y) | (x, y) <- zip (head sorted) (last sorted)] where sorted = map sort strList
-  print ("part 1: " ++ show part1)
-  let part2 = sum [read x * getCountIn x ys | x <- xs]
+  let part1 = sum $ map (\(x, y) -> abs (read x - read y)) $ zip col1 col2
         where
-          getCountIn x xs = sum [1 | y <- xs, x == y]
-          [xs, ys] = strList
-  print ("part 2: " ++ show part2)
+          [col1, col2] = map sort [(map fst ls), (map snd ls)]
+          ls = map ((\([x, y]) -> (x, y)) . words) $ lines contents
+  print part1
 
-getStrList :: String -> [[String]]
-getStrList xs = toList $ unzip $ map (toTuple . words) (lines xs)
-  where
-    toTuple [x, y] = (x, y)
-    toList (x, y) = [x, y]
+  let part2 = sum $ map (\c1 -> read c1 * (length $ filter (== c1) (map snd ls))) $ map fst ls
+        where
+          ls = map ((\([x, y]) -> (x, y)) . words) $ lines contents
+  print part2
