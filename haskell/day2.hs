a6ab599ead2661127f1fe3cc8bed72c316f88292
tmduddy <login@tylerduddy.com>
2024-12-20 16:03:22 -0500

    haskell day2 better

--- haskell/day2.hs
+++ haskell/day2.hs
@@ -1,22 +1,17 @@
-import Data.List (sort)
+import Data.List (partition)
 
 main = do
   contents <- readFile "../days/day2/input.txt"
-  let rs = map ((map readInt) . words) $ lines contents
-      safeRs = filter isSafe rs
-      unsafeRs = filter (not . isSafe) rs
-      isSafe xs = isGradual xs && (sort xs == xs || sort xs == reverse xs)
-      isGradual x
-          | x == [] = True
-          | length x == 1 = True
-      isGradual (x : y : ys) = x /= y && abs (x - y) <= 3 && isGradual (y : ys)
-      permutationsMinusOne _ [] ys = ys
-      permutationsMinusOne i xs ys
-          | i == (length xs) = ys
-          | otherwise = permutationsMinusOne (i + 1) xs (ys ++ [deleteAt i xs])
+  let rs = map ((map read) . words) $ lines contents
+      (safeRs, unsafeRs) = partition isSafe rs
+      isSafe xs = isGradual xs && (or $ zipWith ($) [isAsc, isDesc] (repeat xs))
+        where 
+          isAsc xs = and $ (zipWith (<) <*> (drop 1)) xs
+          isDesc xs = and $ (zipWith (>) <*> (drop 1)) xs
+          isGradual [] = True
+          isGradual (_ : []) = True
+          isGradual (x : y : ys) = x /= y && abs (x - y) <= 3 && isGradual (y : ys)
+      listMinusOne xs = zipWith deleteAt (take (length xs) [0..]) (repeat xs)
           where deleteAt idx zs = let (l, (_ : r)) = splitAt idx zs in l ++ r
   print $ "Part 1: " <> show (length safeRs)
-  print $ "Part 2: " <> show ((length safeRs) + (length $ filter (any isSafe) $ map (\r -> permutationsMinusOne 0 r []) unsafeRs))
-
-readInt :: String -> Int
-readInt x = read x
+  print $ "Part 2: " <> show ((length safeRs) + (length $ filter (any isSafe) $ map listMinusOne unsafeRs))
